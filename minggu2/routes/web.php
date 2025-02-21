225ffa1347953e03b89c0d02ad08884838b17a74
Najwaalya <najwaalya999@gmail.com>
2025-02-21 15:09:33 +0700

    Routing

--- minggu2/routes/web.php
+++ minggu2/routes/web.php
@@ -19,6 +19,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+}); 
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+}); 
+
+Route::get('/about', function(){
+    return 'NIM: 2341720230 <br> Nama: Najwa Alya Nurizzah';
+});
+
+Route::get('/user/{name}', function($name){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function($name=null){
+    return 'Nama saya '.$name;
+});
+
 Route::resource('items', ItemController::class);
 // Mendefinisikan rute untuk resource 'items', yang otomatis menghubungkan berbagai 
 // metode HTTP dengan metode pada ItemController (index, create, store, show, edit, update, destroy)
