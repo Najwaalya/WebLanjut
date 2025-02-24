a698a4bab3321de4cd4feb5fa12e49f359412e18
Najwaalya <najwaalya999@gmail.com>
2025-02-24 09:17:45 +0700

    POS

---
+++ POS/resources/views/products.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products - POS</title>
+</head>
+<body>
+    <h1>Daftar Produk - {{ $category }}</h1>
+    <ul>
+        <li>Produk 1</li>
+        <li>Produk 2</li>
+        <li>Produk 3</li>
+    </ul>
+</body>
+</html>
