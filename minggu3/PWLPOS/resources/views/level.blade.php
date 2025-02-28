d8e7de396a654b39c32c3cfaf6b21afb2a3f8d60
Najwaalya <najwaalya999@gmail.com>
2025-02-28 23:47:39 +0700

    fixed blade

--- minggu3/PWLPOS/resources/views/level.blade.php
+++ minggu3/PWLPOS/resources/views/level.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Data Level Pengguna</h1>
-    <table>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Kode Level</th>
