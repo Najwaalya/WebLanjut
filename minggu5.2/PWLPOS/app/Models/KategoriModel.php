0b16966e619adef171c835201e2193f3b7a1be63
Najwaalya <najwaalya999@gmail.com>
2025-03-23 05:28:01 +0700

    add tugas m_barang

--- minggu5.2/PWLPOS/app/Models/KategoriModel.php
+++ minggu5.2/PWLPOS/app/Models/KategoriModel.php
@@ -9,7 +9,7 @@
  {
      protected $table = "m_kategori";        // Mendefinisikan nama tabel yang digunakan oleh model ini
  
-     protected $primaryKey = "id_kategori";      // Mendefinisikan primary key
+     protected $primaryKey = "kategori_id";      // Mendefinisikan primary key
  
      protected $fillable = ["kategori_kode", "kategori_nama"];
  }
