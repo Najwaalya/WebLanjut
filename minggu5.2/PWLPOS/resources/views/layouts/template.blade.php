0989e41aec8dd387bd1a16d0a0d2e5cb322872f6
Najwaalya <najwaalya999@gmail.com>
2025-03-22 18:02:22 +0700

    add praktikum 2: penerapan layouting

--- minggu5.2/PWLPOS/resources/views/layouts/template.blade.php
+++ minggu5.2/PWLPOS/resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{ config('app.name', PWL Laravel Starter Code') }}</title>
+  <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -36,7 +36,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include('laouts.breadcrumb')
+    @include('layouts.breadcrumb')
 
     <!-- Main content -->
     <section class="content">
--- minggu5.2/PWLPOS/routes/web.php
+++ minggu5.2/PWLPOS/routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
@@ -17,3 +21,4 @@ Route::get('/user/tambah', [UserController::class, 'tambah']);
  Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
  Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
  Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
