61aede1d6953f474c92e820af83f2a5a5cb1d770
Najwaalya <najwaalya999@gmail.com>
2025-04-09 12:20:40 +0700

    add praktikum 1 jobsheet 7: authentication

--- PWLPOS/routes/web.php
+++ PWLPOS/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -107,4 +108,15 @@ Route::group(['prefix' => 'barang'], function () {
      Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
      Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // menghapus data barang (ajax)
      Route::delete('/{id}', [BarangController::class, 'destroy']);         // menghapus data barang
+ });
+
+ Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter (id), maka harus berupa angka
+  
+ Route::get('login', [AuthController::class, 'login'])->name('login');
+ Route::post('login', [AuthController::class, 'postlogin']);
+ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+ 
+ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+ 
+     Route::get('/', [WelcomeController::class, 'index']);
  });
