4f0d32a9d84a0d6d36199bc9f256fa70840abb83
Najwaalya <najwaalya999@gmail.com>
2025-04-26 23:31:46 +0700

    add praktikum 1 JS 11 (Eloquent Accessor)

--- PWLPOS/app/Models/UserModel.php
+++ PWLPOS/app/Models/UserModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -23,7 +24,7 @@ class UserModel extends Authenticatable implements JWTSubject
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'foto', 'created_at', 'updated_at'];
+    protected $fillable = ['username', 'password', 'image', 'nama', 'level_id', 'foto', 'created_at', 'updated_at'];
 
     protected $hidden = ['password']; // jangan ditampilkan saat select
 
@@ -36,6 +37,13 @@ class UserModel extends Authenticatable implements JWTSubject
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' .$image),
+        );
     }
 
     public function getRoleName(): string
